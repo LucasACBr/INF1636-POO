ab7534d55bb485b3f601b41d4895991911b8ddeb
LucasACBr <lucas@projetoacbr.com.br>
2021-12-01 14:33:41 -0300

    Create InicarJogo.java

---
+++ Jogo/src/Iniciar/InicarJogo.java
@@ -0,0 +1,20 @@
+package Iniciar;
+
+import View.*;
+import Model.*;
+public class InicarJogo {
+
+	public static void main(String[] args) {
+		Regras regras = new Regras(0,2);
+		Dados a = new Dados();
+		
+		TelaInicial inicial = new TelaInicial();
+		 			// TODO Auto-generated method stub
+		 inicial.setVisible(true);
+
+		 
+		 
+			 
+	}
+
+}
